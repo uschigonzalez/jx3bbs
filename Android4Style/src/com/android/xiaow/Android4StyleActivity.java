8dc1773cf3f97cfa336e17ec79982c455d0215a0
xw8852 <xw8852@126.com>
2012-07-08 15:00:46 +0000

    初始导入。

---
+++ Android4Style/src/com/android/xiaow/Android4StyleActivity.java
@@ -0,0 +1,14 @@
+package com.android.xiaow;
+
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Android4StyleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
